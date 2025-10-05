530082a410d2a5dc62e10cb8cdb3d8d78f91b2bb
Halibroda <galibrodaa@gmail.com>
2025-10-05 11:58:03 +0300

    Cors added for front

--- src/main/java/com/epam/rd/autocode/spring/project/conf/SecurityConfig.java
+++ src/main/java/com/epam/rd/autocode/spring/project/conf/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable());
+        http.cors(c -> {});
         http.authorizeHttpRequests(reg -> reg
             .requestMatchers(HttpMethod.GET, "/api/books/**").permitAll()
             .requestMatchers("/api/books/**").hasRole("EMPLOYEE")
