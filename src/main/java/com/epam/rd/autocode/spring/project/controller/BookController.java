8d660278654d38ee6510578e596d212e9d47f4ae
Halibroda <galibrodaa@gmail.com>
2025-10-12 12:18:03 +0300

    Home is now page with list of books

--- src/main/java/com/epam/rd/autocode/spring/project/controller/BookController.java
+++ src/main/java/com/epam/rd/autocode/spring/project/controller/BookController.java
@@ -1,56 +1,19 @@
 package com.epam.rd.autocode.spring.project.controller;
 
-import com.epam.rd.autocode.spring.project.dto.BookDTO;
-import com.epam.rd.autocode.spring.project.model.enums.AgeGroup;
-import com.epam.rd.autocode.spring.project.model.enums.Language;
 import com.epam.rd.autocode.spring.project.service.BookService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.math.BigDecimal;
-import java.util.Map;
 
 @Controller
-@RequestMapping("/books")
+@RequestMapping("/book")
 @RequiredArgsConstructor
 public class BookController {
 
     private final BookService bookService;
-
-    @GetMapping
-    public String list(@RequestParam(required = false) String genre,
-                       @RequestParam(required = false) Language language,
-                       @RequestParam(required = false) AgeGroup ageGroup,
-                       @RequestParam(required = false) BigDecimal minPrice,
-                       @RequestParam(required = false) BigDecimal maxPrice,
-                       @RequestParam(required = false, name = "search") String search,
-                       @PageableDefault(size = 12, sort = "name") Pageable pageable,
-                       Model model) {
-
-        Page<BookDTO> page = bookService.findBooks(
-            genre, language, ageGroup, minPrice, maxPrice, search, pageable
-        );
-
-        model.addAttribute("page", page);
-        model.addAttribute("params", Map.of(
-            "genre", genre,
-            "language", language,
-            "ageGroup", ageGroup,
-            "minPrice", minPrice,
-            "maxPrice", maxPrice,
-            "search", search
-        ));
-
-        return "books/list";
-    }
 
     @GetMapping("/{name}")
     public String details(@PathVariable String name, Model model) {
--- src/main/java/com/epam/rd/autocode/spring/project/controller/HomeController.java
+++ src/main/java/com/epam/rd/autocode/spring/project/controller/HomeController.java
@@ -1,13 +1,58 @@
 package com.epam.rd.autocode.spring.project.controller;
 
+import com.epam.rd.autocode.spring.project.dto.BookDTO;
+import com.epam.rd.autocode.spring.project.model.enums.AgeGroup;
+import com.epam.rd.autocode.spring.project.model.enums.Language;
+import com.epam.rd.autocode.spring.project.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final BookService bookService;
+
     @GetMapping("/")
-    public String home() {
+    public String home(@RequestParam(required = false) String genre,
+                       @RequestParam(required = false) Language language,
+                       @RequestParam(required = false) AgeGroup ageGroup,
+                       @RequestParam(required = false) BigDecimal minPrice,
+                       @RequestParam(required = false) BigDecimal maxPrice,
+                       @RequestParam(required = false, name = "search") String search,
+                       @PageableDefault(size = 12, sort = "name") Pageable pageable,
+                       Model model) {
+
+        Page<BookDTO> page;
+        try {
+            page = bookService.findBooks(genre, language, ageGroup, minPrice, maxPrice, search, pageable);
+        } catch (Throwable ignore) {
+            List<BookDTO> all = bookService.getAllBooks();
+            page = new PageImpl<>(all, pageable, all.size());
+        }
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("genre", genre);
+        params.put("language", language);
+        params.put("ageGroup", ageGroup);
+        params.put("minPrice", minPrice);
+        params.put("maxPrice", maxPrice);
+        params.put("search", search);
+
+        model.addAttribute("page", page);
+        model.addAttribute("params", params);
         return "index";
     }
 }
-
